896c208840341e46e29f23b8be010a1e4dd42ce2
Sachin <124561877+ssachi12@users.noreply.github.com>
2023-12-14 15:35:31 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# main.py
+from kivy.app import App
+from kivy.uix.button import Button
+
+class TestApp(App):
+    def build(self):
+        return Button(text='Hello, Kivy!')
+
+if __name__ == '__main__':
+    TestApp().run()
+
